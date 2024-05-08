ca68bf3a60e4fc8806e2ef56a7df95513f0314ca
1aguna <mlusher28@gmail.com>
2024-05-07 22:26:45 -0700

    Testing tokio async processing

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,40 @@
 // Uncomment this block to pass the first stage
 use std::{
     io::{Read, Write},
-    net::TcpListener
 };
-fn main() {
+
+use tokio::net::{TcpListener, TcpSocket};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+fn process(stream: &mut TcpStream) {
+    loop {
+        let mut buf: [u8; 512] = [0; 512];
+        let read_count = stream.read(&mut buf).unwrap();
+        
+        if read_count == 0 {
+            return;
+        }
+        stream.write(b"+PONG\r\n").unwrap();
+    }
+}
+
+
+#[tokio::main]
+async fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
     
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
-    for stream in listener.incoming() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    loop {
+        let stream: Result<(tokio::net::TcpStream, std::net::SocketAddr), std::io::Error> = listener.accept().await;
         match stream {
-            Ok(mut stream) => {
-                loop {
-                    let mut buf: [u8; 512] = [0; 512];
-                    let read_count = stream.read(&mut buf).unwrap();
-
-                    if read_count == 0 {
-                        break;
-                    }
-                    stream.write(b"+PONG\r\n").unwrap();
-                }
-
+            Ok((mut stream, _)) => {
+                // move stream into newly spawned task
+                tokio::spawn(async move {
+                    process(&mut stream);
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
